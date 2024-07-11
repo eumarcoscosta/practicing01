0cf48831c6f14457a1b1b381ce9d638df4afca6b
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-07-11 17:48:18 +0100

    complete app

---
+++ practicing01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero, i;
+            int[] vet;
+
+            Console.WriteLine("Insira o numero abaixo: ");
+
+            numero = int.Parse(Console.ReadLine());
+            vet = new int[numero];
+
+
+            string[] vet2 = Console.ReadLine().Split(' ');
+
+            for (i = 0; i < numero; i++)
+            {
+                vet[i] = int.Parse(vet2[i]);
+            }
+
+            for (i = 0; i < numero; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+
+        }
+    }
+}
